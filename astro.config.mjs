9fd83f171e07cda09a0cca8c2e6d35f9b54ab391
bschm0622 <rfcooper95@gmail.com>
2025-08-01 09:59:59 -0400

    favicons and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:4321",
+  site: "https://beckyschmidt.me",
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
